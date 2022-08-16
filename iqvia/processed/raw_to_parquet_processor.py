90018ffd6b711f3b258fa0d47125eafce038526e
Manny Bacolas <manny.bacolas@gmail.com>
2022-08-16 14:18:40 -0400

    ...

--- iqvia/processed/raw_to_parquet_processor.py
+++ iqvia/processed/raw_to_parquet_processor.py
@@ -2,7 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.context import SparkContext
 from pyspark.conf import SparkConf
 
-from iqvia.common.schema import *
+import json
 from iqvia.common.load import *
 
 spark = SparkSession.builder \
@@ -13,8 +13,14 @@ conf = spark.conf
 
 infer_file_schema = eval(conf.get("spark.nyec.infer_schema", 'False'))
 file_parsing_delimiter = conf.get("spark.nyec.file_delimiter", ',')
-load_paths = conf.get("spark.nyec.ingest_path").split(',')
+dynamic_config = conf.get("spark.nyec.dynamic_config")
+dynamic_config_list = json.loads(dynamic_config)
 
-for p in load_paths:
-    load_df(spark, p, raw_plan_schema, file_delimiter=file_parsing_delimiter, infer_schema=infer_file_schema) \
-        .write.parquet(f'{p}/parquet', mode='overwrite')
+
+for cfg in dynamic_config_list:
+    ingest_path = cfg.get('ingest_path')
+    from_module = cfg.get('from')
+    import_path = cfg.get('import')
+    import_path_f = __import__(import_path, fromlist=(from_module))
+    load_df(spark, ingest_path, import_path_f, file_delimiter=file_parsing_delimiter, infer_schema=infer_file_schema) \
+        .write.parquet(f'{ingest_path}/parquet', mode='overwrite')
